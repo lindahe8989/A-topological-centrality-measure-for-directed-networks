5fdc2ab8fce9ec4e3f8b977eba0e9ec6477fa512
lindahe8989 <82330374+lindahe8989@users.noreply.github.com>
2021-12-21 15:15:05 -0500

    Add files via upload

---
+++ quasi-cen.py
@@ -0,0 +1,25 @@
+file = open("2011 asia gamma.tex", 'r')
+total = 0
+print('origin')
+for i in file.readlines():
+    #print(i)
+    if i=='\n':
+        continue
+    elif 'i=' in i:
+        number_i = i.split('=')[1]
+        print(total)
+        print(i)
+        total = 0
+        continue
+    list_2 = i.split(')')
+    #print(list_2[0].split(','))
+    try:
+        decimal = float(list_2[0].split(',')[1])
+        if decimal==float('inf'):
+            continue
+        total+=decimal
+    except:
+        print(i)
+        print(list_2[0].split(','))
+    #print(decimal)
+print(total)
